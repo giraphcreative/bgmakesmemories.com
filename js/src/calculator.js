a2099ee3ddf251cd23f335ea1258a3608a1d53cc
James Pederson <james@jpederson.com>
2019-03-19 03:22:58 -0500

    Fix the calculator so it works in the meantime. Still waiting on rate.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -3,48 +3,18 @@
 
 $(function(){
 
-	var updateTotal = function() {	
-		var resultOne = parseFloat( $(".result.one").html().replace( '$', '' ) );
-		var resultTwo = parseFloat( $(".result.two").html().replace( '$', '' ) );
-		var resultThree = parseFloat( $(".result.three").html().replace( '$', '' ) );
-		if ( isNaN( resultOne ) ) {
-			resultOne = 0;
-		}
-		if ( isNaN( resultTwo ) ) {
-			resultTwo = 0;
-		}
-		if ( isNaN( resultThree ) ) {
-			resultThree = 0;
-		}
-		var totalSavings = resultOne + resultTwo + resultThree;
-		if ( totalSavings === 'NaN' ) {
-			totalSavings = 0;
-		}
-		$(".result-total-savings").html( totalSavings );
-	}
-
-	$(".calculator.home-equity-one").accrue({
+	$(".calculator.vacation").accrue({
 		mode: "compare",
-		response_output_div: ".result.one",
-		response_compare:"$%savings%",
-		error_text:"",
-		callback: updateTotal
-	});
-
-	$(".calculator.home-equity-two").accrue({
-		mode: "compare",
-		response_output_div: ".result.two",
-		response_compare:"$%savings%",
-		error_text:"",
-		callback: updateTotal
-	});
-
-	$(".calculator.home-equity-three").accrue({
-		mode: "compare",
-		response_output_div: ".result.three",
-		response_compare:"$%savings%",
-		error_text:"",
-		callback: updateTotal
+		response_output_div: ".results.vacation",
+		response_compare: "%savings%",
+		error_text: "",
+		callback: function( elem, nums ) {
+			console.log( elem );
+			console.log( nums );
+			if ( typeof( nums.savings ) === 'undefined' ) {
+				$( ".results.vacation" ).html( '0' );
+			}
+		}
 	});
 
 	$(".numbers-only").keyup(function(){
